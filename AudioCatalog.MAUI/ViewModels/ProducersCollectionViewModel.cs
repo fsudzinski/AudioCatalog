9445ea57bedb463097c15cfcf0c3cba0e2f07753
Filip Sudziński <filip.sudzinski@student.put.poznan.pl>
2024-01-28 03:11:40 +0100

    Search bar and early picker

--- AudioCatalog.MAUI/ViewModels/ProducersCollectionViewModel.cs
+++ AudioCatalog.MAUI/ViewModels/ProducersCollectionViewModel.cs
@@ -7,10 +7,21 @@ namespace Sudzinski.AudioCatalog.MAUI.ViewModels
     public partial class ProducersCollectionViewModel : ObservableObject
     {
         private readonly BLC.BLC _blc;
-        public ICommand OpenAddProducerPageCommand { get; }
 
         [ObservableProperty]
+        private string searchText;
+        [ObservableProperty]
+        private string selectedCountry;
+        [ObservableProperty]
         private ObservableCollection<ProducerViewModel> producers;
+
+        public List<string> Countries { set; get; }
+        private string countriesPickerPlaceholder = "All";
+
+
+        public ICommand OpenAddProducerPageCommand { get; }
+        public ICommand SearchCommand { get; }
+        //public ICommand FilterByCountryCommand { get; }
 
         public ProducersCollectionViewModel(BLC.BLC blc)
         {
@@ -21,21 +32,54 @@ namespace Sudzinski.AudioCatalog.MAUI.ViewModels
             LoadData();
 
             OpenAddProducerPageCommand = new Command(OnOpenAddProducerPage);
+            SearchCommand = new Command<string>(PerformSearch);
+            //FilterByCountryCommand = new Command(FilterByCountry);
         }
 
         public void LoadData()
         {
-            producers.Clear();
+            Producers.Clear();
 
             foreach (var producer in _blc.GetAllProducers())
             {
-                producers.Add(new ProducerViewModel(producer, _blc));
+                Producers.Add(new ProducerViewModel(producer, _blc));
             }
+
+            Countries = Producers.Select(p => p.CountryOfOrigin).Distinct().ToList();
+            Countries.Insert(0, countriesPickerPlaceholder);
         }
 
         private void OnOpenAddProducerPage()
         {
             App.Current.MainPage.Navigation.PushAsync(new AddProducerPage(new AddProducerViewModel(_blc)));
-        }        
+        }
+        private void PerformSearch(string searchText)
+        {
+            LoadData();
+            if (!string.IsNullOrWhiteSpace(searchText))
+            {            
+                searchText = searchText.ToLowerInvariant();
+
+                var filteredProducers = new ObservableCollection<ProducerViewModel>(
+                    Producers.Where(p => p.Name.ToLowerInvariant().Contains(searchText))
+                );
+
+                Producers = filteredProducers;
+            }
+        }
+        private void FilterByCountry(object sender, EventArgs e)
+        {
+            LoadData();
+            if (!string.Equals(SelectedCountry, countriesPickerPlaceholder))
+            {
+                SelectedCountry = SelectedCountry.ToLowerInvariant();
+
+                var filteredProducers = new ObservableCollection<ProducerViewModel>(
+                    Producers.Where(p => p.CountryOfOrigin.ToLowerInvariant() == SelectedCountry)
+                );
+                
+                Producers = filteredProducers;
+            }
+        }
     }
 }
